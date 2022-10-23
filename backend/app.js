a233b69ef74bc086fca3b5239eb2fe99e576cf04
“Kyungmin <rudalslee7@gmail.com>
2022-10-23 18:14:41 -0400

    finish express routing

--- backend/app.js
+++ backend/app.js
@@ -1,20 +1,19 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var express = require("express");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+
+var usersRouter = require("./routes/api/users");
+var tweetsRouter = require("./routes/api/tweets");
 
 var app = express();
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/api/users", usersRouter);
+app.use("/api/tweets", tweetsRouter);
 
 module.exports = app;
