18eb492547da228592fabccb6008ea64207aac8c
“Kyungmin <rudalslee7@gmail.com>
2022-10-23 20:41:48 -0400

    add Passport JWT Setup

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -4,6 +4,8 @@ const bcrypt = require("bcryptjs");
 const mongoose = require("mongoose");
 const User = mongoose.model("User");
 const passport = require("passport");
+const { loginUser, restoreUser } = require("../../config/passport");
+const { isProduction } = require("../../config/keys");
 
 /* GET users listing. */
 router.get("/", function (req, res, next) {
@@ -11,17 +13,26 @@ router.get("/", function (req, res, next) {
     message: "GET /api/users",
   });
 });
+router.get("/current", restoreUser, (req, res) => {
+  if (!isProduction) {
+    const csrfToken = req.csrfToken();
+    res.cookie("CSRF-TOKEN", csrfToken);
+  }
+  if (!req.user) return res.json(null);
+  res.json({
+    _id: req.user._id,
+    username: req.user.username,
+    email: req.user.email,
+  });
+});
 
 // POST /api/users/register
 router.post("/register", async (req, res, next) => {
-  // Check to make sure no one has already registered with the proposed email or
-  // username.
   const user = await User.findOne({
     $or: [{ email: req.body.email }, { username: req.body.username }],
   });
 
   if (user) {
-    // Throw a 400 error if the email address and/or email already exists
     const err = new Error("Validation Error");
     err.statusCode = 400;
     const errors = {};
@@ -35,7 +46,6 @@ router.post("/register", async (req, res, next) => {
     return next(err);
   }
 
-  // Otherwise create a new user
   const newUser = new User({
     username: req.body.username,
     email: req.body.email,
@@ -48,7 +58,7 @@ router.post("/register", async (req, res, next) => {
       try {
         newUser.hashedPassword = hashedPassword;
         const user = await newUser.save();
-        return res.json({ user });
+        return res.json(await loginUser(user));
       } catch (err) {
         next(err);
       }
@@ -65,7 +75,7 @@ router.post("/login", async (req, res, next) => {
       err.errors = { email: "Invalid credentials" };
       return next(err);
     }
-    return res.json({ user });
+    return res.json(await loginUser(user));
   })(req, res, next);
 });
 
