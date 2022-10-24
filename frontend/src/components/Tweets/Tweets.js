10a215e9c4873d44cbb51e91a18a0ecf117031a0
“Kyungmin <rudalslee7@gmail.com>
2022-10-24 07:01:10 -0400

    found bug

---
+++ frontend/src/components/Tweets/Tweets.js
@@ -0,0 +1,26 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { clearTweetErrors, fetchTweets } from "../../store/tweets";
+import TweetBox from "./TweetBox";
+
+function Tweets() {
+  const dispatch = useDispatch();
+  const tweets = useSelector((state) => Object.values(state.tweets));
+  useEffect(() => {
+    dispatch(fetchTweets());
+    return () => dispatch(clearTweetErrors());
+  }, [dispatch]);
+
+  if (tweets.length === 0) return <div>There are no Tweets</div>;
+
+  return (
+    <>
+      <h2>All Tweets</h2>
+      {tweets.map((tweet, i) => (
+        <TweetBox key={i} text={tweet.text} username={tweet.author.username} />
+      ))}
+    </>
+  );
+}
+
+export default Tweets;
